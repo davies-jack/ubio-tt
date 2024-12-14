f8c8cd8f8e0031c466659682083ed5f744801edf
davies-jack <jackethdavies@hotmail.com>
2024-12-14 01:02:23 +0000

    Add schema from framework

--- src/main/schemas/InstanceSchema.ts
+++ src/main/schemas/InstanceSchema.ts
@@ -1,8 +1,31 @@
-export interface InstanceSchema {
-    id: string;
-    group: string;
-    createdAt: number;
-    updatedAt: number;
+import { Schema } from "@ubio/framework";
 
-    meta?: any;
+export interface InstanceSchema {
+  id: string;
+  group: string;
+  createdAt: number;
+  updatedAt: number;
+
+  meta?: {
+    [key: string]: any;
+  };
 }
+
+export const InstanceSchema = new Schema<InstanceSchema>({
+  schema: {
+    type: "object",
+    properties: {
+      id: { type: "string" },
+      group: { type: "string" },
+      createdAt: { type: "number" },
+      updatedAt: { type: "number" },
+      meta: {
+        type: "object",
+        optional: true,
+        properties: {},
+        additionalProperties: true,
+      },
+    },
+    required: ["id", "group", "createdAt", "updatedAt"],
+  },
+});
