de37399047d5b7ff1b48ec498846e3440118bfa9
davies-jack <jackethdavies@hotmail.com>
2024-12-15 17:47:07 +0000

    Add createdInstances method in test instance repository

--- src/main/repositories/test/Instance.ts
+++ src/main/repositories/test/Instance.ts
@@ -13,4 +13,20 @@ export class TestInstanceRepository {
       await this.collection.deleteMany({});
     }
   }
+
+  async createInstances() {
+    if (process.env.NODE_ENV === "test") {
+      await this.collection.insertMany([
+        { group: 'particle-accelerator', id: '123', meta: { location: 'NL' }, createdAt: new Date(), updatedAt: new Date() },
+        { group: 'particle-accelerator', id: '124', meta: { location: 'UK' }, createdAt: new Date(), updatedAt: new Date() },
+        { group: 'particle-accelerator', id: '125', meta: { location: 'US' }, createdAt: new Date(), updatedAt: new Date() },
+        { group: 'particle-accelerator', id: '126', meta: { location: 'DE' }, createdAt: new Date(), updatedAt: new Date() },
+        { group: 'particle-accelerator', id: '127', meta: { location: 'FR' }, createdAt: new Date(), updatedAt: new Date() },
+        { group: 'particle-accelerator', id: '128', meta: { location: 'ES' }, createdAt: new Date(), updatedAt: new Date() },
+        { group: 'particle-accelerator', id: '129', meta: { location: 'IT' }, createdAt: new Date(), updatedAt: new Date() },
+        { group: 'particle-accelerator', id: '130', meta: { location: 'BE' }, createdAt: new Date(), updatedAt: new Date() },
+        { group: 'spaceship-os', id: '131', meta: { location: 'NL' }, createdAt: new Date(), updatedAt: new Date() },
+      ]);
+  }
+  }
 }
